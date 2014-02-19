d72c576530be8ef49bd95c71864be8b4bfac1b5c
mimikrmvrr <mimi_9204@abv.bg>
2014-02-19 15:00:18 +0200

    Add links hash for anchors

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-PAGE_URL = "http://www.mediawiki.org/wiki/Manual:Upgrading"
+PAGE_URL = "http://www.mediawiki.org/wiki/Installation"
 
 class Url
   def initialize(url)
@@ -17,6 +17,14 @@ end
 
 BASE_URL = Url.new(PAGE_URL).base_url
 
+def get_links(page)
+  links = {}
+  page.xpath('//a[@href]').each do |link|
+    links[link.text.strip] = link['href']
+  end
+  links
+end
+
 page = Nokogiri::HTML(open(PAGE_URL))
 page_title = page.title
 h1 = page.css('h1').text
@@ -24,13 +32,14 @@ redirected_from = page.css('div#contentSub').text
 page.at('#toc').remove
 page.css('span[class="mw-editsection"]').map(&:remove)
 content = page.css('div#mw-content-text').text
+links = get_links(page.css('div#mw-content-text'))
 parsed_file_name = "parsed.txt"
 begin
   file = File.open(parsed_file_name, "w")
   file.write(h1)
   file.write(redirected_from)
   file.write(content)
-  #file.write(BASE_URL)
+  #file.write(links)
 rescue IOError => e
   #some error occur, dir not writable etc.
 ensure
--- tests.rb
+++ tests.rb
@@ -8,4 +8,15 @@ class TestSimpleNumber < Test::Unit::TestCase
     assert_equal("https://www.google.bg", Url.new("https://www.google.bg/search?q=google&oq=goo&aqs=chrome.1.69i57j0j69i60j0l2j69i61.2699j0j1&client=ubuntu-browser&sourceid=chrome&ie=UTF-8").base_url)
   end
 
+  def test_get_links
+    html = "<html>
+    <body>
+        <a href=#foo>Foo</a>
+        <a href=#bar>Bar </a>
+    </body>
+</html>"
+    expected_links = {"Foo"=>"#foo", "Bar"=>"#bar"}
+    assert_equal(expected_links, get_links(Nokogiri::HTML(html)))
+  end
+
 end
