d97407a6b9141dc01a095b13cb98987bed38dfe7
mimikrmvrr <mimi_9204@abv.bg>
2014-02-20 15:06:57 +0200

    Start making Page class

--- init.rb
+++ init.rb
@@ -13,9 +13,15 @@ class Url
     uri = URI.parse(@url)
     "#{uri.scheme}://#{uri.host}"
   end
+
+  def path()
+    uri = URI.parse(@url)
+    "#{uri.path}"
+  end
 end
 
 BASE_URL = Url.new(PAGE_URL).base_url
+DATA_DIR = "data/pages"
 
 def get_links(page)
   links = {}
@@ -29,11 +35,21 @@ def internal_links(links)
   links.select { |link_name, link| link.start_with?('/', '#') }
 end
 
-def get_absolute_urls(links)
-  absolute_links = links.map do |link_name, link|
-    link.start_with?('#') ? "#{PAGE_URL}#{link}" : "#{BASE_URL}#{link}"
+def get_absolute_url(link)
+  link.start_with?('#') ? "#{PAGE_URL}#{link}" : "#{BASE_URL}#{link}"
+end
+
+class Page
+  def initialize(url)
+    @html = Nokogiri::HTML(open(url))
+    @name
   end
-  absolute_links.uniq
+
+  def neighbours()
+    links = get_links(@html.css('div#mw-content-text'))
+    @neighbours = internal_links(links).values
+  end
+
 end
 
 page = Nokogiri::HTML(open(PAGE_URL))
@@ -44,14 +60,31 @@ page.at('#toc').remove
 page.css('span[class="mw-editsection"]').map(&:remove)
 content = page.css('div#mw-content-text').text
 links = get_links(page.css('div#mw-content-text'))
-internal = get_absolute_urls(internal_links(links))
+hrefs = internal_links(links)
+
+hrefs.map do |href_name, href|
+  absolute_url = get_absolute_url(href)
+  file_name = "#{DATA_DIR}/#{File.basename(href, '.html')}"
+  unless File.exists?(file_name)
+    begin
+      content = open(absolute_url).read
+      rescue Exception=>e
+      puts "Error: #{e}"
+    else
+      file = File.open(file_name, 'w')
+      file.write(content)
+    end
+  end    
+end
+
 parsed_file_name = "parsed.txt"
+
 begin
   file = File.open(parsed_file_name, "w")
   file.write(h1)
   file.write(redirected_from)
   file.write(content)
-  file.write(internal)
+  #file.write(internal)
 rescue IOError => e
   #some error occur, dir not writable etc.
 ensure
--- tests.rb
+++ tests.rb
@@ -8,6 +8,11 @@ class TestSimpleNumber < Test::Unit::TestCase
     assert_equal("https://www.google.bg", Url.new("https://www.google.bg/search?q=google&oq=goo&aqs=chrome.1.69i57j0j69i60j0l2j69i61.2699j0j1&client=ubuntu-browser&sourceid=chrome&ie=UTF-8").base_url)
   end
 
+  def test_uri_path
+    assert_equal("/wiki/Installation", Url.new("http://www.mediawiki.org/wiki/Installation").path)
+    assert_equal("/search", Url.new("https://www.google.bg/search?q=google&oq=goo&aqs=chrome.1.69i57j0j69i60j0l2j69i61.2699j0j1&client=ubuntu-browser&sourceid=chrome&ie=UTF-8").path)
+  end
+
   def test_get_links
     html = "<html>
     <body>
@@ -33,14 +38,19 @@ class TestSimpleNumber < Test::Unit::TestCase
   end
 
   def test_absolute_urls
-    links = {"Foo"=>"#foo", "Bar"=>"#bar", "Main page"=>'/main'}
-    expected_urls = ['http://www.mediawiki.org/wiki/Installation#foo', 'http://www.mediawiki.org/wiki/Installation#bar', 'http://www.mediawiki.org/main']
-    assert_equal(expected_urls, get_absolute_urls(links))
+    #links = {"Foo"=>"#foo", "Bar"=>"#bar", "Main page"=>'/main'}
+    #expected_urls = ['http://www.mediawiki.org/wiki/Installation#foo', 'http://www.mediawiki.org/wiki/Installation#bar', 'http://www.mediawiki.org/main']
+    assert_equal('http://www.mediawiki.org/wiki/Installation#foo', get_absolute_url("#foo"))
+    assert_equal('http://www.mediawiki.org/wiki/Installation#bar', get_absolute_url("#bar"))
+    assert_equal('http://www.mediawiki.org/main', get_absolute_url("/main"))
+    assert_equal('http://www.mediawiki.org/foo', get_absolute_url("/foo"))
   end
 
-  def test_absolute_urls_uniq
-    links = {"Foo"=>"#foo", "Bar"=>"#bar", "Main page"=>'/main', "Main"=>'/main', "IntenalFoo"=>"/foo", "Foobar"=>'#foo'}
-    expected_urls = ['http://www.mediawiki.org/wiki/Installation#foo', 'http://www.mediawiki.org/wiki/Installation#bar', 'http://www.mediawiki.org/main', 'http://www.mediawiki.org/foo']
-    assert_equal(expected_urls, get_absolute_urls(links))
-  end
+  # def test_absolute_urls_uniq
+  #   links = {"Foo"=>"#foo", "Bar"=>"#bar", "Main page"=>'/main', "Main"=>'/main', "IntenalFoo"=>"/foo", "Foobar"=>'#foo'}
+  #   expected_urls = ['http://www.mediawiki.org/wiki/Installation#foo', 'http://www.mediawiki.org/wiki/Installation#bar', 'http://www.mediawiki.org/main', 'http://www.mediawiki.org/foo']
+  #   assert_equal('http://www.mediawiki.org/wiki/Installation#foo', "#foo")
+  #   assert_equal('http://www.mediawiki.org/wiki/Installation#bar', "#bar")
+  #   assert_equal('http://www.mediawiki.org/main', "/main")
+  # end
 end
