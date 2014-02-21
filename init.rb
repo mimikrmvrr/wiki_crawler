770b77936a9a240e890b2e0d4fa178bbd68e864f
mimikrmvrr <mimi_9204@abv.bg>
2014-02-21 18:54:29 +0200

    Add bfs counter

--- init.rb
+++ init.rb
@@ -12,12 +12,31 @@ print "Enter a url: "
 PAGE_URL = gets.chomp
 BASE_URL = Url.new(PAGE_URL).base_url
 
+print "Depth level:"
+level = gets.to_i
+
 page = Page.new(PAGE_URL)
-page.create_local_file
 
-text = ""
-File.open(page.file_name) { |file|  text = file.read }
+#puts page
 
-counter = Counter.new text
+frequencies = Hash.new(0)
 
-puts counter.frequencies.first 20
+queue = [page]
+until queue.empty?
+  current_page = queue.shift
+  #puts current_page.html
+  if current_page.level <= level
+    current_page.create_local_file
+    text = ""
+    File.open(current_page.file_name) { |file|  text = file.read }
+    counter = Counter.new text
+    #puts counter.frequencies
+    frequencies.merge!(counter.frequencies) { |word, current_count, new_count| current_count + new_count }
+    #puts frequencies
+    queue << current_page.neighbours
+    queue.flatten!
+  else
+    break
+  end
+end
+puts frequencies.sort_by { |word, count| -count }.first 20
--- utils.rb
+++ utils.rb
@@ -23,9 +23,12 @@ end
 class LinkUtils
   def self.get_links(page)
     links = {}
-    page.xpath('//a[@href]').each do |link|
+    page.xpath("//*[@class='mw-pt-languages-list autonym']").map(&:remove) if page.xpath("//*[@class='mw-pt-languages-list']")
+    #page.css("div#mw-content-text").remove.xpath("//*[@id='footer-info']")
+    page.css("div#mw-content-text").xpath('//a[@href]').each do |link|
       links[link.text.strip] = link['href']
     end
+    puts links
     links
   end
 
@@ -40,23 +43,26 @@ end
 
 
 class Page
-  attr_reader :name
+  attr_accessor :level
+  attr_reader :name, :html
 
-  def initialize(url)
+  def initialize(url, level=0)
     begin
       @html = Nokogiri::HTML(open(url))
       @name = Url.new(url).path
+      #create_local_file
+      @level = level
+      puts "Open #{url}"
     rescue Exception=>e
       puts "Cannot open #{url}"
     end
   end
 
   def neighbours
-    links = LinkUtils.get_links(@html.css('div#mw-content-text'))
+    links = LinkUtils.get_links(@html)
     neighbours = LinkUtils.internal_links(links).values.map do |name|
       if ObjectSpace.each_object(Page).select { |obj| obj.name == name }.empty?
-        #puts get_absolute_url(name)
-        Page.new(LinkUtils.get_absolute_url(name))
+        Page.new(LinkUtils.get_absolute_url(name), @level + 1)
       else
         ObjectSpace.each_object(Page).select { |obj| obj.name == name }.first
       end
@@ -71,6 +77,7 @@ class Page
       file.write(heading)
       file.write(content)
     rescue IOError => e
+      puts "Writing error"
       #some error occur, dir not writable etc.
     ensure
       file.close unless file == nil
@@ -82,8 +89,10 @@ class Page
   end
 
   def remove_useless
-    @html.at('#toc').remove
-    @html.css('span[class="mw-editsection"]').map(&:remove)
+    @html.at('#toc').remove if @html.at('#toc')
+    @html.at_css('td[class="mw-pt-languages-label"]').remove if @html.at_css('td[class="mw-pt-languages-label"]')
+    @html.at_css('td[class="mw-pt-languages-list"]').remove if @html.at_css('td[class="mw-pt-languages-list"]')
+    @html.css('span[class="mw-editsection"]').map(&:remove) if @html.css('span[class="mw-editsection"]')
   end
 
   def content
@@ -154,46 +163,7 @@ class Counter
   def frequencies
     frequencies = Hash.new(0)
     @words.each { |word| frequencies[word] += 1 unless WORDS_TO_IGNORE.include? word }
-    frequencies.sort_by { |word, count| count }.reverse
+    frequencies
   end
 end
 
-
-# page = Nokogiri::HTML(open(PAGE_URL))
-# page_title = page.title
-# h1 = page.css('h1').text
-# redirected_from = page.css('div#contentSub').text
-# page.at('#toc').remove
-# page.css('span[class="mw-editsection"]').map(&:remove)
-# content = page.css('div#mw-content-text').text
-#links = get_links(page.css('div#mw-content-text'))
-#hrefs = internal_links(links)
-
-# hrefs.map do |href_name, href|
-#   absolute_url = get_absolute_url(href)
-#   file_name = "#{DATA_DIR}/#{File.basename(href, '.html')}"
-#   unless File.exists?(file_name)
-#     begin
-#       content = open(absolute_url).read
-#       rescue Exception=>e
-#       puts "Error: #{e}"
-#     else
-#       file = File.open(file_name, 'w')
-#       file.write(content)
-#     end
-#   end    
-# end
-
-# parsed_file_name = "parsed.txt"
-
-# begin
-#   file = File.open(parsed_file_name, "w")
-#   file.write(h1)
-#   file.write(redirected_from)
-#   file.write(content)
-#   #file.write(internal)
-# rescue IOError => e
-#   #some error occur, dir not writable etc.
-# ensure
-#   file.close unless file == nil
-# end
