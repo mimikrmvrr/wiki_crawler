8815ac0466c0c984a7d3ddb65e9d4a39bc38d6df
mimikrmvrr <mimi_9204@abv.bg>
2014-02-21 13:13:28 +0200

    Count word frequencies

--- init.rb
+++ init.rb
@@ -9,8 +9,17 @@ DATA_DIR = "data/pages"
 
 print "Enter a url: "
 
-PAGE_URL = gets
+PAGE_URL = gets.chomp
 BASE_URL = Url.new(PAGE_URL).base_url
 
 page = Page.new(PAGE_URL)
 page.create_local_file
+
+text = ""
+File.open(page.file_name) { |file|  text = file.read }
+words = text.split(%r{\W+})
+
+frequencies = Hash.new(0)
+words.each { |word| frequencies[word] += 1 }
+
+puts frequencies.sort_by(&:last)
